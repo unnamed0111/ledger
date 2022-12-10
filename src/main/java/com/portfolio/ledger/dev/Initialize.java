bea79e6af2b3ebefc9fd7a219fc89aec670d1bd9
unknown <hsccomsen@gmail.com>
2022-12-10 23:58:17 +0900

    read 페이지 디자인 html, css, js 일부 작성

--- src/main/java/com/portfolio/ledger/dev/Initialize.java
+++ src/main/java/com/portfolio/ledger/dev/Initialize.java
@@ -73,7 +73,9 @@ package com.portfolio.ledger.dev;
 
 import com.portfolio.ledger.domain.Member;
 import com.portfolio.ledger.domain.MemberRole;
+import com.portfolio.ledger.dto.AccountDTO;
 import com.portfolio.ledger.repository.MemberRepository;
+import com.portfolio.ledger.service.AccountService;
 import lombok.Cleanup;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -85,6 +87,7 @@ import org.springframework.stereotype.Component;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.Statement;
+import java.time.LocalDate;
 import java.util.stream.IntStream;
 
 @Log4j2
@@ -97,12 +100,16 @@ public class Initialize implements ApplicationRunner {
     private MemberRepository memberRepository;
 
     @Autowired
+    private AccountService accountService;
+
+    @Autowired
     private PasswordEncoder passwordEncoder;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
         createLoginTable();
         createUsers();
+        createAccounts();
     }
     private void createLoginTable() throws Exception {
         log.info("............CREATE TABLE PERSISTENT FOR LOGINS............");
@@ -138,5 +145,21 @@ public class Initialize implements ApplicationRunner {
 
             memberRepository.save(member);
         });
+    }
+
+    private void createAccounts() {
+        log.info("..........................CREATE ACCOUNTS..........................");
+
+        AccountDTO accountDTO = AccountDTO.builder()
+                .date(LocalDate.now())
+                .title("테스트 내역 1")
+                .content("테스트 내역입니다. 단가 100원 수량 100개")
+                .price(100.0)
+                .amount(100)
+                .writer("user1")
+                .snp(true)
+                .build();
+
+        accountService.register(accountDTO);
     }
 }
